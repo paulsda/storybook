c6fc555786ff98074f49d61c6a5b9ddce617ac4c
Luis Carrazco <luiscarrazco@gmail.com>
2019-08-26 11:06:34 -0600

    static build

---
+++ storybook-static/precache-manifest.d29467b5c86180676acb6d733285b9ff.js
@@ -0,0 +1,23 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f13941d9a188d7bcd15c816d7afc9c",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "1380b069270ab7047778",
+    "url": "main.css"
+  },
+  {
+    "url": "main.de6652a1b6756e460fbd.bundle.js"
+  },
+  {
+    "url": "runtime~main.de6652a1b6756e460fbd.bundle.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "url": "vendors~main.de6652a1b6756e460fbd.bundle.js"
+  }
+]);
