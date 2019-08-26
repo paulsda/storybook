aea36e3d9a2c825d7628feabc1eff97e6f0a0b75
Luis Carrazco <luiscarrazco@gmail.com>
2019-08-26 11:03:09 -0600

    Initial setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +23,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
